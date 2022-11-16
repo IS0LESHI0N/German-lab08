24449c8d33c310d62e0cf344ec3a22b9eaa96195
IS0LESHI0N <114649518+IS0LESHI0N@users.noreply.github.com>
2022-11-16 22:33:15 +0200

    Add files via upload

---
+++ lab08/dist/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "mps",
+  1: "m",
+  2: "mps"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Functions"
+};
+
